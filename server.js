6992ee29d6c6879debebe060107b0fb0aff5a705
shiva-chaganti <sivatej.vtz059@gmail.com>
2015-10-06 23:16:17 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 8989));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+    response.render('web');
+});
+
+app.get('/data', function(request, response) {
+    var data = require('./demo.json');
+    response.json(data);
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+})
